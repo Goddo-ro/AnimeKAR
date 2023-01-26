7452ac8e8a1c079bf6edf510d91597d7e8df22df
Goddo-ro <r0manovic1@yandex.ru>
2023-01-26 20:28:56 +0300

    Add Loader

--- src/pages/Anime.jsx
+++ src/pages/Anime.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useFetching } from "../hooks/useFetching";
 import AnimeService from "../API/AnimeService";
 import AnimeList from "../components/AnimeList";
+import Loader from "../components/UI/Loader/Loader";
 
 const Anime = () => {
   const [anime, setAnime] = useState([]);
@@ -17,7 +18,10 @@ const Anime = () => {
 
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <AnimeList anime={anime} />
+      {isAnimeLoading
+        ? <Loader />
+        : <AnimeList anime={anime} />
+      }
     </div>
   );
 };
