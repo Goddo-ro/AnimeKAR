7cf232c7f82f0a590495240e4a47515689e51cc1
Goddo-ro <r0manovic1@yandex.ru>
2023-02-05 21:26:40 +0300

    Add all general info

--- src/pages/AnimeById.jsx
+++ src/pages/AnimeById.jsx
@@ -4,6 +4,7 @@ import { useFetching } from "../hooks/useFetching";
 import AnimeService from "../API/AnimeService";
 import Loader from "../components/UI/Loader/Loader";
 import { getFormattedDate } from "../utils/dateFormating";
+import { capitalize } from "../utils/stringUtils";
 
 const AnimeById = ({ children }) => {
   const params = useParams();
@@ -39,7 +40,25 @@ const AnimeById = ({ children }) => {
     { title: "Type", body: anime.type },
     { title: "Episodes", body: anime.episodes },
     { title: "Status", body: anime.status },
-    { title: "Aired", body: `from ${getFormattedDate(anime.aired?.from)} to ${getFormattedDate(anime.aired?.to)}` }
+    { title: "Aired", body: `from ${getFormattedDate(anime.aired?.from)} to ${getFormattedDate(anime.aired?.to)}` },
+    { title: "Premiered", body: `${anime.season && capitalize(anime.season)} ${anime.year}` },
+    { title: "Broadcast", body: anime.broadcast?.string },
+    { title: "Producers", body: anime.producers?.map(producer => producer.name).join(", ") },
+    { title: "Licensors", body: anime.licensors?.map(licensor => licensor.name).join(", ") },
+    { title: "Studios", body: anime.studios?.map(studio => studio.name).join(", ") },
+    { title: "Source", body: anime.source },
+    { title: "Genres", body: anime.genres?.map(genre => genre.name).join(", ") },
+    { title: "Themes", body: anime.themes?.map(theme => theme.name).join(", ") },
+    { title: "Duration", body: anime.duration },
+    { title: "Rating", body: anime.rating }
+  ];
+
+  const statistics_div = [
+    { title: "Score", body: `${anime.score} scored by ${anime.scored_by} users` },
+    { title: "Ranked", body: `#${anime.rank}` },
+    { title: "Popularity", body: `#${anime.popularity}` },
+    { title: "Members", body: anime.members },
+    { title: "Favorites", body: anime.favorites },
   ]
 
   return (
@@ -48,7 +67,7 @@ const AnimeById = ({ children }) => {
         <h2>{anime.title}</h2>
       </div>
       <div className="flex">
-        <div className="border-r-2 border-gray-600 mr-2 pr-2">
+        <div className="anime-general-info border-r-2 border-gray-600 mr-2 pr-2">
           <img src={anime.images?.jpg.large_image_url}/>
           <div className="anime-info-section">
             <h3>Alternative Titles</h3>
@@ -63,6 +82,15 @@ const AnimeById = ({ children }) => {
             <h3>Information</h3>
             {
               information_div.map(info =>
+                <p className="text-gray-500">
+                  <a className="text-gray-800">{info.title}</a>: {info.body}
+                </p>)
+            }
+          </div>
+          <div className="anime-info-section">
+            <h3>Statistics</h3>
+            {
+              statistics_div.map(info =>
                 <p className="text-gray-500">
                   <a className="text-gray-800">{info.title}</a>: {info.body}
                 </p>)
---
+++ src/utils/stringUtils.js
@@ -0,0 +1,9 @@
+export const capitalize = (word) => {
+  const firstLetter = word.charAt(0);
+
+  const firstLetterCap = firstLetter.toUpperCase();
+
+  const remainingLetters = word.slice(1);
+
+  return firstLetterCap + remainingLetters;
+}
